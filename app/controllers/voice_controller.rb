9a9652637a2e4daba11ba1e936260d399813b41e
Unknown <thehofch@gmail.com>
2018-05-09 22:15:55 +0300

    improve

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -1,6 +1,11 @@
 class VoiceController < ApplicationController
   def convert
-    file = ConvertTextToSpeech.call(params[:text], 'us-east-1', 'Maxim')
-    send_data(file.audio_stream.read, filename: "#{Time.now}.mp3")
+    file = ConvertTextToSpeech.call(params[:text], 'us-east-1', params[:voice], params[:format])
+    extension = if params[:format] == 'ogg_vorbis'
+                  'ogg'
+                else
+                  params[:format]
+                end
+    send_data(file.audio_stream.read, filename: "#{Time.now}.#{extension}")
   end
 end
--- app/operations/convert_text_to_speech.rb
+++ app/operations/convert_text_to_speech.rb
@@ -1,12 +1,12 @@
 module ConvertTextToSpeech
   extend self
-  def call(text, region, voice_id)
+  def call(text, region, voice_id, format)
     client = Aws::Polly::Client.new(region: region,
                                     access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
                                     secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key)
     )
     client.synthesize_speech(text: text,
-                             output_format: 'mp3',
+                             output_format: format,
                              voice_id: voice_id,
                              text_type: 'text'
     )

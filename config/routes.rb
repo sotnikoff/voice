99bade68dbce17b2b52502241cf22202fe63cea8
Unknown <thehofch@gmail.com>
2018-05-09 23:41:56 +0300

    share ability

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'convert', as: 'convert_voice', controller: 'voice',
                         action: 'convert'
-  post 'show', as: 'show_voice', controller: 'voice', action: 'show'
+  get 'show', as: 'show_voice', controller: 'voice', action: 'show'
 end

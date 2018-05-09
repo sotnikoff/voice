63c57e5c147ce56c5f2d6dc0e8205c5b06051e1c
Unknown <thehofch@gmail.com>
2018-05-09 21:39:54 +0300

    pg

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -1,8 +1,6 @@
 class VoiceController < ApplicationController
   def convert
-    puts params[:text]
     file = ConvertTextToSpeech.call(params[:text], 'us-east-1', 'Maxim')
-    puts file.audio_stream
-    send_data(file.audio_stream.read, filename: 'test.mp3')
+    send_data(file.audio_stream.read, filename: "#{Time.now}.mp3")
   end
 end
